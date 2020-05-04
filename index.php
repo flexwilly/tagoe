1b2c8e1ce8c0ddec23787bcc8d8f7f4791f3732d
flexwilly <onjorew@gmail.com>
2020-05-04 14:21:48 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+<title>Drina Hosting Services</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href = "normal.css" media="all" rel="stylesheet" type = "text/css"/>
+
+</head>
+<body>
+               <div id = "navigation">
+			   <ul>
+               <li><a class="active" href="index.php">Home</a></li>
+               <li><a href="#">About</a></li>
+               </ul>
+               </div>			   
+               <div id = "header">
+                 <h1>Drina Hosting Services</h1> 				 
+              </div>
+			  <div id="main">
+			  
+			  <table id="bronzish" height="100px" width="100px" align="left">
+			  <tr><th>Bronze</th></tr>
+			  <tr><td>
+			  30GB WebHosting Unlimited Emails AccountsUnlimited BandwidthWordpress + 600 CMSFree Lets Encrypt SSLDaily Website BackupUpto 3 Websites Hosting
+			  </td></tr>
+			  </table>
+			  
+			   &nbsp;
+			  <table id="silverish" height="100px" width="100px" align="center">
+			  <tr><th>Silver</th></tr>
+			  <tr><td>
+			  60GB WebHosting Unliited Emails AccountsUnlimited BandwidthWordpress + 800 CMSFree Lets Encrypt SSLDaily Website BackupUpto 3 Websites Hosting
+			  </td></tr>
+			  </table>
+			  &nbsp;
+			  <table id="goldish" height="100px" width="100px" align="right">
+			  <tr><th>Goldish</th></tr>
+			  <tr><td>
+			  90GB WebHosting Unlimited Emails AccountsUnlimited BandwidthWordpress + 1200 CMSFree Lets Encrypt SSLDaily Website BackupUpto 3 Websites Hosting
+			  </td></tr>
+			  </table>
+			 
+			  </div>
+              
+               
+			  
+<div id =  "footer">
+CopyRight <?php echo date("Y");?> Drina Hosting Services
+</div>			   
+</body>
+</html>
+
+
+
+
+           
